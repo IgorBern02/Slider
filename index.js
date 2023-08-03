e8bd1bb3d3a6e8dbdf8c1e757902ce6d38abf067
IgorBern02 <igor.bernardes2@gmail.com>
2023-08-03 00:22:57 -0300

    Slider

---
+++ index.js
@@ -0,0 +1,36 @@
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider(){
+    slider.forEach(item => item.classList.remove('on'));
+};
+
+function showSlider(){
+    slider[currentSlide].classList.add('on');
+};
+
+function prevSlider(){
+    hideSlider();   
+    if(currentSlide === 0){
+        currentSlide = slider.length -1
+    } else {
+        currentSlide--
+    }
+    showSlider()
+};
+
+function nextSlider(){
+    hideSlider();
+    if(currentSlide === slider.length -1){
+        currentSlide = 0
+    } else {
+        currentSlide++
+    }
+    showSlider()
+};
+
+btnPrev.addEventListener('click', prevSlider);
+btnNext.addEventListener('click', nextSlider);
